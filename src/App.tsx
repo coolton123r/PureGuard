8ee8841d83d30f6aec9f68c437b525b8f3a744cc
coolton123r <hiblercolton37@gmail.com>
2026-01-17 11:31:29 -0600

    Create App component with header and message

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>PureGuard</h1>
+      <p>You are not alone. This app is here to help.</p>
+    </div>
+  )
+}
+
+export default App
 
